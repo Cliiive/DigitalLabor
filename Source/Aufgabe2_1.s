d3476b205d418991c79b396ebd19536dc0a61eea
Jonas Sasowski <sasowski@outlook.de>
2024-04-13 13:42:36 +0200

    fixed version Aufgabe2_1

--- Source/Aufgabe2_1.s
+++ Source/Aufgabe2_1.s
@@ -3,8 +3,8 @@
  *
  * SoSe 2024
  *
- *  Created on: <$Date>
- *      Author: <$Name>
+ *  Created on: <13.04.2024>
+ *      Author: <Jonas Sasowski>
  *
  *	Aufgabe : 64 Bit Addition
  */
@@ -21,15 +21,15 @@ main:
   mov r3,# (1 << 31) //Higher-Word
 
   //Starten mit der Addition
-  add r4, r0, r2 //Lower-Word Ergebnis
+  adds r4, r0, r2 //Lower-Word Ergebnis
   mov r7,#0 
   movcs r7,#1 //Überlauf temporär speichern falls C Flag = 1
 
-  add r5, r1, r2 //Higher-Word Ergebnis
+  adds r5, r1, r2 //Higher-Word Ergebnis
   mov r8,#0
   movcs r8,#1 //Überlauf temporär speichern falls C Flag = 1
 
-  add r5, r7, r5 //Überlauf von Lower mit Higher addieren & überlauf speichern
+  adds r5, r7, r5 //Überlauf von Lower mit Higher addieren & überlauf speichern
   mov r9,#0
   movcs r9,#1
 
