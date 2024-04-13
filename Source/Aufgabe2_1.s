5f4f183f062915b4e91522ad1ac6a0517b4ea96d
Jonas Sasowski <sasowski@outlook.de>
2024-04-13 13:26:20 +0200

    implementation Aufgabe2_1

--- Source/Aufgabe2_1.s
+++ Source/Aufgabe2_1.s
@@ -12,6 +12,28 @@
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
 main:
+  //Zahl 1
+  mov r0,# (1 << 31) //Lower-Word
+  mov r1,# (1 << 31)  //Higher-Word
+
+  //Zahl 2
+  mov r2,# (1 << 31) //Lower-Word
+  mov r3,# (1 << 31) //Higher-Word
+
+  //Starten mit der Addition
+  add r4, r0, r2 //Lower-Word Ergebnis
+  mov r7,#0 
+  movcs r7,#1 //Überlauf temporär speichern falls C Flag = 1
+
+  add r5, r1, r2 //Higher-Word Ergebnis
+  mov r8,#0
+  movcs r8,#1 //Überlauf temporär speichern falls C Flag = 1
+
+  add r5, r7, r5 //Überlauf von Lower mit Higher addieren & überlauf speichern
+  mov r9,#0
+  movcs r9,#1
+
+  add r6, r9, r8 //Beide Überläufe addieren
 
 
 stop:
