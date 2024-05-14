f821e67231a4ee3d16bfc5940c8ebdf27034872c
Jonas Sasowski <sasowski@outlook.de>
2024-05-14 20:57:01 +0200

    added files to gitignore & implemented aufgabe 3_1

--- Source/Aufgabe3_1.s
+++ Source/Aufgabe3_1.s
@@ -3,15 +3,33 @@
  *
  * SoSe 2024
  *
- *  Created on: <$Date>
- *      Author: <$Name>
+ *  Created on: <14.05.2024>
+ *      Author: <Jonas Sasowski>
  *
  *	Aufgabe : Unterprogrammaufruf
  */
 .text /* Specify that code goes in text segment */
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
+
+.equ DELAY_CONSTANT, 3
+
 main:
+  ldr r0,=4 //Testwert
+  bl delay_function
+  ldr r0,=5 //Testwert
+  b stop
+
+delay_function:
+
+  stmfd sp!, {r0, lr} //Speichern von dem Arbeitsregister (falls es gerade in benutzung is)
+  ldr r0, =DELAY_CONSTANT
+
+  loop:
+    subs r0, r0, #1
+    bne loop
+  
+  ldmfd sp!, {r0, pc}
 
 
 stop:
