5a371c0941f983e0601489e4fa71a251417d0eb4
Jonas Sasowski <sasowski@outlook.de>
2024-05-14 22:40:59 +0200

    added 3_3

--- Source/Aufgabe3_2.s
+++ Source/Aufgabe3_2.s
@@ -3,8 +3,8 @@
  *
  * SoSe 2024
  *
- *  Created on: <$Date>
- *      Author: <$Name>
+ *  Created on: <14.05.2024>
+ *      Author: <Jonas Sasowski>
  *
  *	Aufgabe : Unterprogrammaufruf  mit Parametern
  */
--- Source/Aufgabe3_3.s
+++ Source/Aufgabe3_3.s
@@ -3,8 +3,8 @@
  *
  * SoSe 2024
  *
- *  Created on: <$Date>
- *      Author: <$Name>
+ *  Created on: <14.05.2024>
+ *      Author: <Jonas Sasowski>
  *
  *	Aufgabe : Unterprogrammaufruf  mit Parameterübergabe über dem Stack
  */
@@ -12,6 +12,22 @@
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
 main:
+  ldr r6,=3
+
+  push {r6} //Parameter auf dem Stack speichern
+  bl delay_function
+  
+  add sp, sp, #4 // Stack berreinigen (von dem Parameter r6)
+
+delay_function:
+
+  push {r0, lr} //Speichern von dem Arbeitsregister
+  ldr r0, [sp, #8]  // Lade den Parameter vom Stack in r0 (Offset 8 wegen push => sp ist gerade auf lr)
+  loop:
+    subs r0, r0, #1
+    bne loop
+  
+  pop {r0, pc}
 
 
 stop:
