390ea694b7f744f78a599d27505c27e273552343
Jonas Sasowski <sasowski@outlook.de>
2024-05-14 21:51:36 +0200

    added 2_3 to 3_1

--- Source/Aufgabe3_1.s
+++ Source/Aufgabe3_1.s
@@ -14,15 +14,42 @@
 
 .equ DELAY_CONSTANT, 3
 
-main:
-  ldr r0,=4 //Testwert
-  bl delay_function
-  ldr r0,=5 //Testwert
-  b stop
+//Daten
+data:
+  .word 0x1
+  .word 0x2
+  .word 0x3
+  .word 0x4
+  .word 0x5
+  .word 0x6
+  .word 0x7
+  .word 0x8
 
+main:
+  mov r0,#0
+  mov r4,#8
+  mov r2,#0
+  ldr r5,=data
+
+  while:
+    ldr r1, [r5]
+    add r5, r5, #4
+    cmp r1, r2
+    movgt r1,#1
+    movle r1,#0
+    mov r0, r0, lsl#4
+    orr r0, r0, r1
+    
+    bl delay_function
+
+    subs r4,#1
+    bne while
+  
+  b stop
+  
 delay_function:
 
-  stmfd sp!, {r0, lr} //Speichern von dem Arbeitsregister (falls es gerade in benutzung is)
+  stmfd sp!, {r0, lr} //Speichern von dem Arbeitsregister
   ldr r0, =DELAY_CONSTANT
 
   loop:
@@ -30,7 +57,6 @@ delay_function:
     bne loop
   
   ldmfd sp!, {r0, pc}
-
 
 stop:
 	nop
