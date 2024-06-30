84eb10c26d0b7d777e5037f37bd36699a5fa8a4e
Jonas Sasowski <sasowski@outlook.de>
2024-06-30 17:58:45 +0200

    Aufgabe 7_2

--- Source/Aufgabe7_2.c
+++ Source/Aufgabe7_2.c
@@ -9,8 +9,37 @@
  *	Aufgabe : GPIO in C
  */
 #include <stdint.h>
-#include "Aufgabe7_2.h"
+#include "LPC21XX.H"
 
-int main(void) 
-{
+#define OUTPUT_MASK 0x00FF0000
+#define LAMP0_MASK (1<<16)
+#define DELAY_PARAMETER_9 
+#define DELAY_PARAMETER_1 
+
+void configure() {
+  IODIR1 = OUTPUT_MASK; //Alle Lampen auf Output Setzen
+}
+
+void delay(uint32_t delayParameter) {
+  while (delayParameter != 0) {
+    delayParameter--;
+  }
+}
+
+void runlight() {
+  while (1) {
+    uint32_t mask = LAMP0_MASK;
+    for (uint8_t i=0; i<8; i++) {
+      IOSET1 = IOSET1 | mask;
+      delay(0x900000);
+      IOCLR1 = mask;
+      delay(0x100000);
+      mask = (mask << 1);
+    }
+  }
+}
+
+int main(void) {
+  configure();
+  runlight();
 }
