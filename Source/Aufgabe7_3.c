da6123c2d5d74f8c0aabfc17439faa71d324fca6
Jonas Sasowski <sasowski@outlook.de>
2024-07-01 00:13:37 +0200

    All working, only minor improvements to do

--- Source/Aufgabe7_3.c
+++ Source/Aufgabe7_3.c
@@ -17,7 +17,6 @@
 #define BUTTON_2_bm (1<<12)
 #define BUTTON_3_bm (1<<13)
 
-uint8_t noButtonPressed();
 
 void configure() {
   IODIR1 = OUTPUT_MASK; //Alle Lampen auf Output Setzen
@@ -31,43 +30,36 @@ void delay(uint32_t delayParameter) {
 }
 
 uint8_t processKeys() {
-  if (~IOPIN0 & BUTTON_0_bm) {
+  if ((~IOPIN0 & (BUTTON_0_bm | BUTTON_2_bm)) == (BUTTON_0_bm | BUTTON_2_bm)) {
+    //delay(0x900000);
+    return 4;
+  }
+  else if (~IOPIN0 & BUTTON_0_bm) {
     return 0;
   }
   else if (~IOPIN0 & BUTTON_1_bm) {
     return 1;
   }
   else if (~IOPIN0 & BUTTON_2_bm) {
+    delay(0x90000);
     return 2;
   }
   else if (~IOPIN0 & BUTTON_3_bm) {
     return 3;
-  }
-  else if (~IOPIN0 & (BUTTON_0_bm | BUTTON_2_bm)) {
-    return 4;
   }
   else {
     return 5;
   }
 }
 
-uint8_t noEscapePressed() {
-  uint8_t action = processKeys();
-  if (action == 5) {
-    return 1;
-  } 
-  else {
-    return 0;
-  }
-}
-
 void runLightLeft(uint8_t counter, int *mask) {
     if (counter == 0) {
-      *mask = (1<<23);
+      *mask = (1<<24);
     }
+    //IOCLR1 = *mask;
     IOCLR1 = *mask;
-    IOSET1 = IOSET1 | *mask;
     *mask = (*mask>>1);
+    IOSET1 = IOSET1 | *mask;
 }
 
 void runLightRight(uint8_t counter, int *mask) {
@@ -78,8 +70,6 @@ void runLightRight(uint8_t counter, int *mask) {
     IOCLR1 = *mask;
     *mask = (*mask<<1);
     IOSET1 = IOSET1 | *mask;
-    
-    
 }
 
 
@@ -87,23 +77,42 @@ void runLightRight(uint8_t counter, int *mask) {
 void runProgram() {
   uint8_t isRunning = 0;
   uint8_t counter = 0;
+  uint8_t direction = 0;
   int *mask;
-  int value = (1<<15);
-  mask = &value;
+  int defaultMask = (1<<15);
+  mask = &defaultMask;
 
   while (1) {
     uint8_t action = processKeys();
 
     if (action == 0) {isRunning = 1;}
-    else if (action == 1) {break;}
-    else if (action == 2) {isRunning = 0;}
-
-    if (isRunning) {
-      runLightRight(counter, mask);
-      counter = (counter+1)%9;
+    else if (action == 1) {IOCLR1 = OUTPUT_MASK;break;} //Terminate the Program
+    else if (action == 2) {isRunning = !isRunning;} //Pause/Continue
+    else if (action == 3) { //Reset the Program
+      isRunning = 0; 
+      counter = 0;
+      direction = 0;
+      int *mask;
+      int defaultMask = (1<<15);
+      mask = &defaultMask;
+      IOCLR1 = OUTPUT_MASK;
+    }
+    else if (action == 4) { //Change direction
+    isRunning = 1; direction = !direction;
     }
 
+    if (isRunning) {
+      if (direction == 0) {
+        runLightRight(counter, mask);
+      }
+      else {
+        runLightLeft(counter, mask);
+      }
+      counter = (counter+1)%9;
+    }
+    
     delay(0x99999);
+    
   }
   
 }
